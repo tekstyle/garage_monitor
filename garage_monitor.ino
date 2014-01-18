d6adc6ef0211ccd232e253238aeceec319c04caf
adiaz004 <adiaz004@yahoo.com>
2014-01-18 13:35:05 -0500

    removed old code and added comments.

--- garage_monitor.ino
+++ garage_monitor.ino
@@ -1,91 +1,87 @@
 
-#include "pitches.h"
-int motion_sensor = 2;  // IC pin 4
-int door_sensor = 4;    // IC pin 6
-int red_led = 3;        // IC pin 5
-int green_led = 5;      // IC pin 11
-int blue_led = 6;     // IC pin 12
-int relay = 9;         // IC pin 15
-int power = 13;         // IC pin 19
-int interrupt = 0;
-int counter = 0;
-int WAIT_TIME = 20; // 20 secs
-int pulse_time = 100;
-boolean message_sent = false;
-volatile int wait_time = 300000; // 300000 = 5 min timer.
+#include <LiquidCrystal.h>
 
-int melody[] = {NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
-int noteDurations[] = {4, 8, 8, 4,4,4,4,4 };
+int relay = 0;          // ATMega 328P pin 2
+int motion_sensor = 1;  // ATMega 328P pin 3
+int door_sensor = 7;    // ATMega 328P pin 13
+int red_led = 8;        // ATMega 328P pin 14
+int blue_led = 9;       // ATMega 328P pin 15
+int green_led = 10;     // ATMega 328P pin 16
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // ATMega 328P pins {18, 17, 11, 6, 5, 4}
+
+int counter = 0;  // each increment of counter will represent 1 second.
+int pulse_time = 100;
+boolean message_sent = false;  // will help keep track if door_close was called.
+
 void setup()
 {
   pinMode (motion_sensor, INPUT);
   pinMode (door_sensor, INPUT);
-  pinMode (power, OUTPUT);
   pinMode (red_led, OUTPUT);
   pinMode (green_led, OUTPUT);
   pinMode (blue_led, OUTPUT);
   digitalWrite (red_led, HIGH);
   digitalWrite (green_led, HIGH);
   digitalWrite (blue_led, HIGH);
-  //attachInterrupt (interrupt, reset_timer, RISING); // motion sensor pin goes high when motion detected.
+
+  lcd.begin (16, 2);
+  lcd.print ("Garage Monior")
+  lcd.setCursor (0, 1);
+  lcd.print ("Version 1.0);
+  delay (2000);
 }
 
 void loop ()
 {
+  // Motion detected and door is open, set RGB to yellow and reset counter.
   if (motion () && !door_closed ())
   {
     setColor (127, 127, 255);
     counter = 0;
   }
   
+  // If door is closed, set RGB to green, reset message_sent and wait till door opens.
   if (door_closed ())
   {
     setColor (255, 127, 255);
     message_sent = false; 
   }
+  // door open, set RGB to red.
   else 
     setColor (127, 255, 255);
-    
+  
+  // If door is open, counter has reached 30 (~30 seconds) and message has not been sent, then close door.  
   if (!door_closed () && counter == 30 && !message_sent)
   {
     pulse (red_led, 5);
     close_door (); 
   }
   
-  if (counter != 30)
+  delay(1000);
+  
+  if (counter != 30) // Wait ~30 seconds before reseting back to zero.
     ++counter;
   else
     counter = 0;
-  delay(1000);
+  
 }
 
+// return true if door closed.
 boolean door_closed ()
 {
  return (digitalRead (door_sensor) == 0); 
 }
 
+// return true if motion detected.
 boolean motion ()
 {
   return (digitalRead (motion_sensor) == 1); 
 }
 
+// close relay for 2 seconds to simulate pushing garage door button.
 void close_door ()
 {
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-    // to calculate the note duration, take one second 
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000/noteDurations[thisNote];
-    tone(8, melody[thisNote],noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(8);
-  }
+  
   digitalWrite (relay, HIGH);
   delay (2000);
   digitalWrite (relay, LOW);
